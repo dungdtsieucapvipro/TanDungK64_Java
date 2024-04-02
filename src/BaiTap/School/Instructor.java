51c9ae4b10aea9f859aef7796731116dce2b2ae5
dungdtsieucapvipro <hoangtandungd5ddt@gmail.com>
2024-04-03 05:34:22 +0700

    bai tap school

---
+++ src/BaiTap/School/Instructor.java
@@ -0,0 +1,36 @@
+package BaiTap.School;
+import java.util.ArrayList;
+import java.util.List;
+public class Instructor {
+    private String name;
+    private List<Course> courses;
+
+    public Instructor(String name) {
+        this.name = name;
+        this.courses = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void add_course(Course course) {
+        courses.add(course);
+    }
+
+    public void remove_course(Course course) {
+        courses.remove(course);
+    }
+}
