fe156b84f076876b84aede06f1bf713391a5da73
dungdtsieucapvipro <hoangtandungd5ddt@gmail.com>
2024-04-03 05:34:53 +0700

    bai tap school

--- src/BaiTap/Person/Teacher.java
+++
@@ -1,9 +0,0 @@
-package BaiTap.Person;
-
-public class Teacher extends Person{
-    private int numCourses;
-    private String[] courses;
-    public Teacher(String name, String address) {
-        super(name, address);
-    }
-}
